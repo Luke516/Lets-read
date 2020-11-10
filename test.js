9cfbbe916542fe32f38b37e83af15887f1f0befe
jackraken <apple894894@gmail.com>
2020-11-11 00:58:58 +0800

    Update test.js

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ console.log("haha QWQ");
 window.a = 10;
 console.log(window.a)
 
-window.parent.a = window.parent.a + 2;
-console.log(window.parent.a)
+parent.a = parent.a + 2;
+console.log('QWQ:' + parent.a)
 
 setTimeout(()=>{
   console.log(window.a)
