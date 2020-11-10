79c113165767c096b931bc4aaf25584a65926bb1
jackraken <apple894894@gmail.com>
2020-11-11 00:01:21 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,3 +2,13 @@
 //     console.log("haha QWQ");
 // }
 console.log("haha QWQ");
+
+window.a = 10;
+console.log(window.a)
+
+window.parent.a = window.parent.a + 2;
+console.log(window.parent.a)
+
+setTimeout(()=>{
+  console.log(window.a)
+}, 5000)
