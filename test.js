8650ca49d07ef240249a5e78e28b25c19199889f
jackraken <apple894894@gmail.com>
2020-11-17 11:07:36 +0800

    Update test.js

--- test.js
+++ test.js
@@ -13,8 +13,10 @@ setTimeout(()=>{
   console.log(window.a)
 }, 5000)
 
-
-const data = { name: 'QWQ' }
-const channel = new MessageChannel()
-window.parent.postMessage(data, [channel.port2])
+setTimeout(()=>{
+  const data = { name: 'QWQ' }
+  const channel = new MessageChannel()
+  window.parent.postMessage(data, [channel.port2])
+  console.log("posted")
+}, 2500)
 
